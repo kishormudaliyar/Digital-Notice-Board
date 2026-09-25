bdcbc2399199142c20141b417a59917d9357477b
Kishor <kishorvijayan394@gmail.com>
2026-09-25 14:22:17 +0530

    refactor: fully remove Survey & Feedback and Student Requirement Survey

--- sw.js
+++ sw.js
@@ -7,11 +7,10 @@
  * 4. Web Push Notification and Notification Click Handlers
  */
 
-const CACHE_NAME = 'notice-board-v3';
+const CACHE_NAME = 'notice-board-v4';
 const STATIC_ASSETS = [
     './',
     './index.html',
-    './survey.html',
     './css/styles.css',
     './js/app.js',
     './manifest.json',
